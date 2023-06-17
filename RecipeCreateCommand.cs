3ae01b9cec157bb9695a96c6d5d439cfe2f2ff45
Владислав Вяткин <vyatin2017@mail.ru>
2023-06-17 20:01:47 +0700

    Implemented the Command pattern: command Create a prescription and undo this creation.

---
+++ RecipeCreateCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace group_project_recipe_management {
+  public class RecipeCreateCommand : ICommand {
+    Recipes recipes;
+
+    public RecipeCreateCommand(Recipes RecipesObj) { 
+      recipes = RecipesObj;
+    }
+
+    public bool Execute(Recipe RecipeObj) {
+      return recipes.CreateRecipe(RecipeObj);
+    }
+
+    public bool Undo(Recipe RecipeObj) {
+      return recipes.DeleteRecipe(RecipeObj);
+    }
+  }
+
+  public interface ICommand {
+    bool Execute(Recipe RecipeObj);
+    bool Undo(Recipe RecipeObj);
+  }
+}
